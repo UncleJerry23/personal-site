673b2b4381109fe1022954ed6256fc8447c6e8b5
UncleJerry <jared.myhrberg@gmail.com>
2019-12-11 12:24:09 -0800

    header location css

--- src/components/header-footer/Header.js
+++ src/components/header-footer/Header.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import styles from './header.css';
 
+const header = {
+  ['']: 'Who is he?',
+  ['about-site']: 'About this site',
+  ['why']: 'Right Side Menu??',
+  ['projects']: 'His Works',
+  ['contact']: 'Contact'
+};
+
+
 const Header = () => {
+  const history = useHistory();
+  let location = history.location.pathname.slice(1);
+
   return (
     <header className={styles.Header} >
-      <h1>jared-Myhrberg: </h1>
-      <span>&#123;</span>
+      <div>
+        <h1>jared-Myhrberg: </h1>
+        <span>&#123;</span>
+      </div>
+      <h3>&gt; {header[location]}</h3>
     </header>
   );
 };
